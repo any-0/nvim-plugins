bdf42e506d0b925b19c12ca171f97a9f2b254e8d
any-0 <julianorlich1@gmail.com>
2025-07-27 14:05:33 +0200

    Transparency, new binds

--- init.lua
+++ init.lua
@@ -2,15 +2,6 @@ require("user.options")
 require("user.keymaps")
 require("user.lazy")
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.defer_fn(function()
-      vim.cmd("NvimTreeToggle")
-    end, 20)
-  end,
-})
-  
 vim.opt.termguicolors = true
 vim.opt.guicursor = ""
-
-vim.opt.shell = 'powershell.exe'
+vim.opt.swapfile = false
--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
   if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -33,43 +34,76 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
       "hrsh7th/cmp-nvim-lsp",
       "L3MON4D3/LuaSnip",
     } },
-    {
-      "nvim-lualine/lualine.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      config = function()
-        require('lualine').setup {
-          sections = {
-            lualine_c = {
-            'filename',
-            lastWriteTime,
-            },
-          },
-          }
-      end,
+
+{
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local theme = require("lualine.themes.gruvbox-material")
+    for _, mode in pairs(theme) do
+      for _, section in pairs(mode) do
+        if type(section) == "table" then
+          section.bg = nil
+          section.gui = nil
+        end
+      end
+    end
+
+    for _, mode_name in ipairs({ "normal", "insert", "visual", "replace", "command", "terminal" }) do
+      if theme[mode_name] and theme[mode_name].a then
+        theme[mode_name].a.fg = "#ffffff"
+      end
+    end
+
+    require("lualine").setup {
+      options = {
+        theme               = theme,
+        component_separators = "",
+        section_separators   = "",
+        globalstatus         = true,
+      },
+      sections = {
+        lualine_a = { { "mode" } },
+        lualine_b = {},
+        lualine_c = { "filename", lastWriteTime },
+        lualine_x = {}, lualine_y = {}, lualine_z = {},
+      },
+    }
+  end,
+},
+{
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_background            = "light"
+    vim.g.gruvbox_material_transparent_background = 1
+    vim.cmd.colorscheme("gruvbox-material")
+  end,
+},
+
+-- ▸ transparent helper
+{
+  "xiyaowong/transparent.nvim",
+  lazy = false,
+  opts = {
+    extra_groups  = {
+      "NvimTreeNormal", "NvimTreeWinSeparator",
+      "LualineNormal", "LualineInsert", "LualineVisual",
+      "LualineReplace", "LualineCommand", "LualineTerminal",
+      "StatusLine", "StatusLineNC" 
     },
-    {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_background = "light"   -- or "hard", "mix", "light"
-      vim.cmd("colorscheme gruvbox-material")
-    end,
-    },
-     { 
-      "mluders/comfy-line-numbers.nvim",
-      config = function()
-        require("comfy-line-numbers").setup({
-          up_key = '<Up>',
-          down_key = '<Down>'
-        })
-      end,
-    },
+    exclude_groups = { "CursorLine" },
+  },
+},
 
   })
 
 
 
-  require("user.plugins.lsp")
-  require("user.plugins.cmp")
-  require("user.plugins.treesitter")
+require("user.plugins.lsp")
+require("user.plugins.cmp")
+require("user.plugins.treesitter")
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#000000", blend = 20 })
+vim.api.nvim_set_hl(0, "StatusLine",   { bg = "none", fg = nil })
+vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none", fg = nil })
